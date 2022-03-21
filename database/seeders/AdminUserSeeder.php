e1e333bb02ed64bac2c047b8a70dcecc0051e03b
vitaliy emelyanov <vitaliy030589@gmail.com>
2022-03-21 13:10:33 +0300

    make admin seeder

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'vmestesmr@gmail.com',
+            'password' => bcrypt('U8ps67UHS'),
+        ]);
+    }
+}
